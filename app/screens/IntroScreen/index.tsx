0b69b4839873c77e74c51db36956791825924d84
Karan Pratap Singh <synester.gates4@gmail.com>
2019-11-03 18:25:17 +0530

    added typescript support

--- app/screens/IntroScreen/index.tsx
+++ app/screens/IntroScreen/index.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
-import { useSelector } from 'react-redux';
-import Logo from '../../../assets/images/react-logo.png';
+import { StyleSheet, Text, View } from 'react-native';
 import { MaterialColors, Typography } from '../../theme';
 
 const IntroScreen = () => {
 
-    const { status } = useSelector(state => state.appData);
-
     return (
         <View style={styles.container}>
             <View style={{ flexDirection: 'row', alignItems: 'center', }}>
-                <Image source={Logo} style={styles.logo} />
                 <View style={styles.headerContainer}>
                     <Text style={styles.heading}>React Native Boilerplate</Text>
                     <Text style={styles.body}>Robust boilerplate to kickstart your next app</Text>
                 </View>
             </View>
-            <Text style={styles.item}>Redux <Text style={{ color: MaterialColors.green[400] }}>{status}</Text></Text>
+            <Text style={styles.item}>Redux <Text style={{ color: MaterialColors.green[400] }}>added</Text></Text>
             <Text style={styles.item}>TypeScript <Text style={{ color: MaterialColors.blue[800] }}>Added</Text></Text>
         </View>
     );
