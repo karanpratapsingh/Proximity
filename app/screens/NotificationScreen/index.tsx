8cf844459d2cf5d15503ff12bdb89639af1f3c77
Karan Pratap Singh <synester.gates4@gmail.com>
2019-11-05 17:23:43 +0530

    notification screen refactor

--- app/screens/NotificationScreen/index.tsx
+++ app/screens/NotificationScreen/index.tsx
@@ -1,9 +1,11 @@
 import React, { useContext } from 'react';
 import { StyleSheet, View } from 'react-native';
+import { responsiveWidth } from 'react-native-responsive-dimensions';
+import { FlatGrid } from 'react-native-super-grid';
 import { ThemeContext } from '../../context/ThemeContext';
-import { Header } from '../../layout';
+import { Header, ListEmptyComponent } from '../../layout';
 import { ThemeColors } from '../../types';
-import NotificationList from './components/NotificationList';
+import NotificationCard from './components/NotificationCard';
 
 const NotificationScreen: React.FC = () => {
 
@@ -12,7 +14,22 @@ const NotificationScreen: React.FC = () => {
   return (
     <View style={styles(theme).container}>
       <Header title='Notifications' />
-      <NotificationList />
+      <FlatGrid
+        itemDimension={responsiveWidth(85)}
+        showsVerticalScrollIndicator={false}
+        items={new Array(10).fill({})}
+        ListEmptyComponent={() => <ListEmptyComponent listType='notifications' spacing={60} />}
+        style={styles().notificationList}
+        spacing={20}
+        renderItem={({ item, index }) => (
+          <NotificationCard
+            avatar='https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80'
+            handle='@amy_234'
+            type={null}
+            time='12hrs'
+          />
+        )}
+      />
     </View>
   );
 };
@@ -21,6 +38,10 @@ const styles = (theme = {} as ThemeColors) => StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.base
+  },
+  notificationList: {
+    flex: 1,
+    paddingHorizontal: 4
   }
 });
 
