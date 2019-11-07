b3d5eb0c4dfb7b91824ace44a655ec530529613a
Karan Pratap Singh <synester.gates4@gmail.com>
2019-11-07 20:51:28 +0530

    added chats query

--- app/graphql/query/index.ts
+++ app/graphql/query/index.ts
@@ -33,3 +33,19 @@ export const QUERY_NOTIFICATION = gql`
     }
   }
 `;
+
+export const QUERY_CHATS = gql`
+  query Chats($userId: String!) {
+    chats(userId: $userId) {
+      participants {
+        id
+        avatar
+        handle
+      }
+      updatedAt
+      messages(last: 1) {
+        body
+      }
+    }
+  }
+`;
