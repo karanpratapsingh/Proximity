3430edd27f0d0cf235c5a4ad87f9a532559da77c
Karan Pratap Singh <synester.gates4@gmail.com>
2019-11-07 16:40:13 +0530

    connected apollo client

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import AppNavigator from './app/navigation';
 import { ThemeContextProvider } from './app/context/ThemeContext';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const client = new ApolloClient({
+  uri: 'https://social-app-node-graphql.herokuapp.com/',
+  cache: new InMemoryCache()
+});
 
 const App = () => (
-  <ThemeContextProvider>
-    <AppNavigator />
-  </ThemeContextProvider>
+  <ApolloProvider client={client}>
+    <ThemeContextProvider>
+      <AppNavigator />
+    </ThemeContextProvider>
+  </ApolloProvider>
 );
 
 export default App;
