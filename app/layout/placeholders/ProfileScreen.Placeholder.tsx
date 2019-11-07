78b91f964db718955b421fa43f487833860aa044
Karan Pratap Singh <synester.gates4@gmail.com>
2019-11-07 17:53:27 +0530

    added profilescreen placeholder

---
+++ app/layout/placeholders/ProfileScreen.Placeholder.tsx
@@ -0,0 +1,81 @@
+import React, { useContext } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Fade, Placeholder, PlaceholderLine, PlaceholderMedia } from 'rn-placeholder';
+import { ThemeContext } from '../../context/ThemeContext';
+import { ThemeColors } from '../../types';
+
+const ProfileScreenPlaceholder: React.FC = () => {
+
+  const { theme } = useContext(ThemeContext);
+
+  return (
+    <View style={styles(theme).container}>
+      <Placeholder Animation={Fade}>
+        <View style={styles().avatarPlaceholder}>
+          <PlaceholderLine style={styles().connectionsPlaceholder} noMargin width={24} height={40} />
+          <PlaceholderMedia size={120} isRound />
+          <PlaceholderLine style={styles().connectionsPlaceholder} noMargin width={24} height={40} />
+        </View>
+        <View style={styles().infoPlaceholder}>
+          <PlaceholderLine style={styles().namePlaceholder} noMargin width={70} height={20} />
+          <PlaceholderLine style={styles().handlePlaceholder} noMargin width={50} height={16} />
+          <PlaceholderLine style={styles().aboutPlaceholder} noMargin width={100} height={110} />
+        </View>
+        <View style={styles().postContainer}>
+          <PlaceholderLine style={styles().postPlaceholder} noMargin width={48.5} height={150} />
+          <PlaceholderLine style={styles().postPlaceholder} noMargin width={48.5} height={150} />
+        </View>
+        <View style={[styles().postContainer, { marginTop: 10 }]}>
+          <PlaceholderLine style={styles().postPlaceholder} noMargin width={48.5} height={150} />
+          <PlaceholderLine style={styles().postPlaceholder} noMargin width={48.5} height={150} />
+        </View>
+      </Placeholder>
+    </View>
+  );
+};
+
+const styles = (theme = {} as ThemeColors) => StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    paddingBottom: 0,
+    backgroundColor: theme.base
+  },
+  avatarPlaceholder: {
+    marginTop: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  connectionsPlaceholder: {
+    borderRadius: 5
+  },
+  infoPlaceholder: {
+    marginTop: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  namePlaceholder: {
+    borderRadius: 5
+  },
+  handlePlaceholder: {
+    marginTop: 10,
+    borderRadius: 5
+  },
+  aboutPlaceholder: {
+    marginTop: 20,
+    borderRadius: 5
+  },
+  postContainer: {
+    marginTop: 10,
+    flexDirection: 'row',
+    // backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  postPlaceholder: {
+    borderRadius: 5
+  }
+});
+
+export default ProfileScreenPlaceholder;
