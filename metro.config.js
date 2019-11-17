cb7b86a71aef029f16c961c0a32b3d18b6a5d5b5
Karan Pratap Singh <synester.gates4@gmail.com>
2019-11-17 11:25:10 +0530

    updated metro config

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,18 @@
-module.exports = {
+const { getDefaultConfig } = require('metro-config');
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts }
+  } = await getDefaultConfig();
+  return {
     transformer: {
-        getTransformOptions: async () => ({
-            transform: {
-                experimentalImportSupport: false,
-                inlineRequires: false,
-            },
-        }),
+      experimentalImportSupport: false,
+      inlineRequires: false,
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
     },
-};
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg']
+    }
+  };
+})();
