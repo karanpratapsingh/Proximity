c1625ae77915c97d9b4e8203fc549031983385ac
Karan Pratap Singh <synester.gates4@gmail.com>
2019-11-21 12:40:09 +0530

    added settings bottomsheet

--- app/screens/ProfileScreen/components/SettingsBottomSheet.tsx
+++ app/screens/ProfileScreen/components/SettingsBottomSheet.tsx
@@ -1,10 +1,15 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import Modalize from 'react-native-modalize';
 import { AppContext } from '../../../context';
 import { BottomSheetHeader } from '../../../layout';
 import { ThemeColors } from '../../../types';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import Checkbox from 'react-native-modest-checkbox';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { ThemeStatic, Typography } from '../../../theme';
+
+const { FontWeights, FontSizes } = Typography;
 
 interface SettingsBottomSheetType {
   ref: React.Ref<any>
@@ -12,7 +17,20 @@ interface SettingsBottomSheetType {
 
 const SettingsBottomSheet: React.FC<SettingsBottomSheetType> = React.forwardRef((props, ref) => {
 
-  const { userId, theme } = useContext(AppContext);
+  const { userId, toggleTheme, theme } = useContext(AppContext);
+
+  const [isChecked, setIsChecked] = useState(false);
+
+  const onChange = ({ checked }) => {
+
+    if (checked) {
+      toggleTheme('dark');
+    } else {
+      toggleTheme('light');
+    }
+
+    setIsChecked(checked);
+  };
 
   return (
     <Modalize
@@ -21,16 +39,22 @@ const SettingsBottomSheet: React.FC<SettingsBottomSheetType> = React.forwardRef(
       scrollViewProps={{
         showsVerticalScrollIndicator: false
       }}
-      modalStyle={styles().container}
+      modalStyle={styles(theme).container}
       adjustToContentHeight>
       <BottomSheetHeader
         heading='Settings'
         subHeading='Themes and options'
       />
       <View style={styles().content}>
-        {/* <TouchableOpacity onPress={() => null}>
-
-        </TouchableOpacity> */}
+        <Checkbox
+          labelBefore
+          checked={isChecked}
+          label='Dark Mode'
+          onChange={onChange}
+          labelStyle={styles(theme).label}
+          checkedComponent={<MaterialIcons name='done' size={25} color={ThemeStatic.accent} />}
+          uncheckedComponent={<MaterialIcons name='done' size={25} color={ThemeStatic.text02} />}
+        />
       </View>
     </Modalize>
   );
@@ -38,12 +62,17 @@ const SettingsBottomSheet: React.FC<SettingsBottomSheetType> = React.forwardRef(
 
 const styles = (theme = {} as ThemeColors) => StyleSheet.create({
   container: {
-    padding: 20
+    padding: 20,
+    backgroundColor: theme.base
   },
   content: {
     flex: 1
   },
-
+  label: {
+    ...FontWeights.Light,
+    ...FontSizes.Body,
+    color: theme.text01
+  }
 });
 
 export default SettingsBottomSheet;
