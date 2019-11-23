9f1607c6327459188451058283c4181a025db997
Karan Pratap Singh <synester.gates4@gmail.com>
2019-11-23 15:57:40 +0530

    say hi to Proximity

--- ios/ProximityTests/ProximityTests.m
+++ ios/ProximityTests/ProximityTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface BoilerplateTests : XCTestCase
+@interface ProximityTests : XCTestCase
 
 @end
 
-@implementation BoilerplateTests
+@implementation ProximityTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
