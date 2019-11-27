ac63301b6b2423fd6df791fc3a76a88953786117
Karan Pratap Singh <synester.gates4@gmail.com>
2019-11-27 17:29:27 +0530

    added post card

---
+++ app/screens/HomeScreen/components/PostCard.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { NativeImage } from '../../../layout';
+import { Typography, ThemeStatic } from '../../../theme';
+
+const { FontWeights, FontSizes } = Typography;
+
+interface PostCardProps {
+  avatar: string,
+  name: string,
+  time: string,
+  uri: string,
+  likes: number,
+  caption: string
+};
+
+const PostCard: React.FC<PostCardProps> = ({ avatar, name, time, uri, likes, caption }) => {
+
+  const navigateToPost = () => { };
+  return (
+    <TouchableOpacity onPress={navigateToPost} activeOpacity={0.9} style={styles.container}>
+      <NativeImage
+        uri={uri}
+        style={styles.postImage}
+      />
+
+      <View style={styles.upperContent}>
+        <NativeImage
+          uri={avatar}
+          style={styles.avatarImage}
+        />
+        <View>
+          <Text style={styles.nameText}>{name}</Text>
+          <Text style={styles.timeText}>{time}</Text>
+        </View>
+      </View>
+
+      <View style={styles.lowerContent}>
+        <Text style={styles.likesText}>{likes} likes</Text>
+        <Text numberOfLines={1} ellipsizeMode='tail' style={styles.captionText}>{caption}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: 400,
+    width: 340,
+    alignSelf: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: ThemeStatic.black,
+    overflow: 'hidden',
+    borderRadius: 10
+  },
+  postImage: {
+    position: 'absolute',
+    height: 400,
+    width: 340
+  },
+  avatarImage: {
+    height: 50,
+    width: 50,
+    borderRadius: 50,
+    marginRight: 12
+  },
+  upperContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: 'rgba(0, 0, 0, 0.1)'
+  },
+  lowerContent: {
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: 'rgba(0, 0, 0, 0.1)'
+  },
+  nameText: {
+    ...FontWeights.Bold,
+    ...FontSizes.Body,
+    color: ThemeStatic.white
+  },
+  timeText: {
+    ...FontWeights.Light,
+    ...FontSizes.Caption,
+    color: ThemeStatic.white,
+    marginTop: 2,
+  },
+  likesText: {
+    ...FontWeights.Bold,
+    ...FontSizes.Body,
+    color: ThemeStatic.white
+  },
+  captionText: {
+    ...FontWeights.Light,
+    ...FontSizes.Body,
+    color: ThemeStatic.white,
+    marginTop: 5
+  }
+});
+
+export default PostCard;
--- app/screens/HomeScreen/index.tsx
+++ app/screens/HomeScreen/index.tsx
@@ -1,12 +1,15 @@
 import React, { useContext } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Image, View, Text, } from 'react-native';
 import { AppContext } from '../../context';
-import { Header } from '../../layout';
+import { Header, NativeImage } from '../../layout';
 import { ThemeColors } from '../../types';
 import { Typography } from '../../theme';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import { useNavigation } from 'react-navigation-hooks';
 import { Routes } from '../../constants';
+import PostCard from './components/PostCard';
+import { FlatGrid } from 'react-native-super-grid';
+import { responsiveWidth } from 'react-native-responsive-dimensions';
 
 const { IconSizes } = Typography;
 
@@ -16,11 +19,30 @@ const HomeScreen: React.FC = () => {
   const { navigate } = useNavigation();
 
   const navigateToMessages = () => navigate(Routes.MessageScreen);
+
+  const renderItem = ({ item }) => <PostCard
+    avatar='https://source.unsplash.com/random'
+    name='Charlotte Jefferson'
+    time='12 hrs ago'
+    uri='https://source.unsplash.com/random'
+    likes={12}
+    caption='I look freakin awesome man'
+  />;
+
   return (
     <View style={styles(theme).container}>
       <Header
         title='Home'
         IconRight={() => <FontAwesome onPress={navigateToMessages} name='send' size={IconSizes.x5} color={theme.text01} />}
+      />
+      <FlatGrid
+        itemDimension={responsiveWidth(85)}
+        showsVerticalScrollIndicator={false}
+        items={[1, 2, 3]}
+        // ListEmptyComponent={() => <ListEmptyComponent listType='messages' spacing={60} />}
+        style={styles().postList}
+        spacing={20}
+        renderItem={renderItem}
       />
     </View>
   );
@@ -30,6 +52,9 @@ const styles = (theme = {} as ThemeColors) => StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.base
+  },
+  postList: {
+    flex: 1
   }
 });
 
