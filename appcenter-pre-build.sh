2ba7a4670689414c6063ea14b4b485a104f3dff4
Karan Pratap Singh <synester.gates4@gmail.com>
2019-11-29 14:57:42 +0530

    enabled google services prebuild inject

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -2,13 +2,13 @@
 
 node node_modules/jetifier/bin/jetify
 
-# if [[ "$APPCENTER_BRANCH" =~ ^(master$|release/.*) ]]; then
+if [[ "$APPCENTER_BRANCH" =~ ^(master$|release/.*) ]]; then
 
-#   if [[ ! -z "$GOOGLE_SERVICES_JSON" ]]; then
-#     echo "$GOOGLE_SERVICES_JSON" | base64 --decode > android/app/google-services.json
-#   fi
+  if [[ ! -z "$GOOGLE_SERVICES_JSON" ]]; then
+    echo "$GOOGLE_SERVICES_JSON" | base64 --decode > android/app/google-services.json
+  fi
 
-#   if [[ ! -z "$GOOGLE_SERVICES_PLIST" ]]; then
-#     echo "$GOOGLE_SERVICES_PLIST" | base64 --decode > ios/GoogleService-Info.plist
-#   fi
-# fi
+  if [[ ! -z "$GOOGLE_SERVICES_PLIST" ]]; then
+    echo "$GOOGLE_SERVICES_PLIST" | base64 --decode > ios/GoogleService-Info.plist
+  fi
+fi
