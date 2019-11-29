cdc2e0599e7873c352fe250f8e2613b1eba799b6
Karan Pratap Singh <synester.gates4@gmail.com>
2019-11-29 10:39:11 +0530

    storage method refactor

--- app/utils/firebase.ts
+++ app/utils/firebase.ts
@@ -1,3 +1,11 @@
 import firebase from 'react-native-firebase';
+import { generateUUID } from './shared';
 
 export const storage = firebase.storage();
+
+export const uploadToStorage = (path: string, uri: string) => {
+  const [type] = uri.split('.').slice(-1);
+  return storage
+    .ref(`${path}/${generateUUID()}.${type}`)
+    .putFile(uri);
+};
