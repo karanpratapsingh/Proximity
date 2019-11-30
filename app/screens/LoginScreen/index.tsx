79baf22c59d1c446cc6ae649062b98590013333a
Karan Pratap Singh <synester.gates4@gmail.com>
2019-11-30 22:45:52 +0530

    basic login button refresh

--- app/screens/LoginScreen/index.tsx
+++ app/screens/LoginScreen/index.tsx
@@ -1,6 +1,6 @@
 import { useMutation } from '@apollo/react-hooks';
 import { GoogleSignin } from '@react-native-community/google-signin';
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import { useNavigation } from 'react-navigation-hooks';
@@ -9,15 +9,20 @@ import { AppContext } from '../../context';
 import client from '../../graphql/client';
 import { MUTATION_CREATE_USER } from '../../graphql/mutation';
 import { QUERY_SIGNIN, QUERY_USER_EXISTS } from '../../graphql/query';
-import { Typography } from '../../theme';
+import { Typography, ThemeStatic } from '../../theme';
 import { ThemeColors } from '../../types';
+import LoginBanner from '../../../assets/svg/login-banner.svg';
+import GoogleLogo from '../../../assets/svg/google-logo.svg';
+import { responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions';
+import { Button } from '../../layout';
 
 const { FontWeights, FontSizes } = Typography;
 
 const LoginScreen: React.FC = () => {
-  const [createUser] = useMutation(MUTATION_CREATE_USER);
   const { theme, updateUser } = useContext(AppContext);
   const { navigate } = useNavigation();
+  const [createUser] = useMutation(MUTATION_CREATE_USER);
+  const [loading, setLoading] = useState(false);
 
   const navigateToApp = async (token: string) => {
     const { data: { signIn: { id, avatar, handle } } } = await client
@@ -35,11 +40,12 @@ const LoginScreen: React.FC = () => {
       SplashScreen.hide();
       if (isSignedIn) {
         const currentUser = await GoogleSignin.getCurrentUser();
-        //@ts-ignore
-        navigateToApp(currentUser.user.id);
+        if (currentUser) {
+          navigateToApp(currentUser.user.id);
+        }
       }
-    } catch (error) {
-      alert(JSON.stringify(error));
+    } catch ({ message }) {
+      alert(JSON.stringify(message));
     }
   };
 
@@ -47,49 +53,66 @@ const LoginScreen: React.FC = () => {
     initialize();
   }, []);
 
-  const onSignIn = async () => {
+  const onGoogleSignIn = async () => {
+
+    if (loading) return;
 
     try {
+      setLoading(true);
       const data = await GoogleSignin.signIn();
       const { user: { id: token, name, photo } } = data;
-
       const { data: { userExists } } = await client.query({ query: QUERY_USER_EXISTS, variables: { token } });
       if (!userExists) {
         await createUser({ variables: { token: token, avatar: photo, name } });
       }
-      //@ts-ignore
+      setLoading(false);
       navigateToApp(token);
-    } catch (error) {
-      alert(JSON.stringify(error));
+    } catch ({ message }) {
+      setLoading(false);
+      alert(JSON.stringify({ message }));
     }
   };
 
   return (
     <View style={styles(theme).container}>
-      <View style={styles(theme).banner}>
+      <View style={styles(theme).content}>
         <Text style={styles(theme).titleText}>Proximity</Text>
         <Text style={styles(theme).subtitleText}>
-          Welcome to a social media where you are more than a statistics
+          Welcome to a open
+          source social media where you are
+          more than a statistics
         </Text>
       </View>
-
-      <TouchableOpacity
-        onPress={onSignIn}
-        style={styles(theme).loginButton}>
-        <Text style={styles(theme).loginButtonText}>Sign in with Google</Text>
-      </TouchableOpacity>
+      <View style={styles(theme).banner}>
+        <LoginBanner />
+        <View>
+          <Button
+            Icon={GoogleLogo}
+            label='Sign in with Google'
+            onPress={onGoogleSignIn}
+            containerStyle={styles(theme).loginButton}
+            labelStyle={styles(theme).loginButtonText}
+            indicatorColor={ThemeStatic.accent}
+            loading={loading}
+          />
+          <TouchableOpacity
+            onPress={() => null}
+            style={styles(theme).terms}>
+            <Text style={styles(theme).termsText}>Terms and conditions</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
 
-const styles = (theme: ThemeColors) => StyleSheet.create({
+const styles = (theme = {} as ThemeColors) => StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-between',
     backgroundColor: theme.base
   },
-  banner: {
-    marginTop: 80,
+  content: {
+    marginTop: 40,
     marginHorizontal: 20
   },
   titleText: {
@@ -103,16 +126,36 @@ const styles = (theme: ThemeColors) => StyleSheet.create({
     marginTop: 10,
     color: theme.text02
   },
-  loginButton: {
+  banner: {
+    flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 20,
-    backgroundColor: theme.accent
+    justifyContent: 'space-between',
+    paddingTop: responsiveHeight(12),
+    paddingBottom: 16,
+  },
+  loginButton: {
+    height: 44,
+    width: responsiveWidth(90),
+    alignSelf: 'center',
+    marginBottom: 10,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: theme.accent,
+    backgroundColor: theme.base
   },
   loginButtonText: {
+    ...FontWeights.Regular,
+    ...FontSizes.Body,
+    marginLeft: 10,
+    color: theme.accent
+  },
+  terms: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  termsText: {
     ...FontWeights.Light,
-    ...FontSizes.Label,
-    color: theme.white
+    ...FontSizes.Body,
+    color: theme.text02
   }
 });
 
