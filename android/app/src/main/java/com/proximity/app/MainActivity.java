27c2a371e6ae3795f37f8caaa7ff39172e033069
Karan Pratap Singh <synester.gates4@gmail.com>
2019-11-30 15:17:49 +0530

    fixed import issue java

--- android/app/src/main/java/com/proximity/app/MainActivity.java
+++ android/app/src/main/java/com/proximity/app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.proximity.app;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
