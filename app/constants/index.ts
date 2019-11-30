ef293c7402116e4299fa791966c9e0a04c2ab72a
Karan Pratap Singh <synester.gates4@gmail.com>
2019-11-30 13:01:58 +0530

    cleanup: dummy users

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -54,10 +54,3 @@ export const HandleAvailableColor = {
   true: 'green',
   false: 'red'
 };
-
-export const DummyUsers = {
-  '@occult_686': 'ck2oj3x2n001w0765e34k94w1',
-  '@ayushieee': 'ck2oj8o2m00290765ckrg3ozy',
-  '@doggo': 'ck2ojhiw1002v0765ou6bdsl8',
-  '@catto': 'ck31eyhnp0dur0723jkme9dyn'
-};
--- app/context/index.tsx
+++ app/context/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState, createContext, } from 'react';
 import { Theme } from '../theme';
 import { ThemeColors } from '../types';
-import { DummyUsers } from '../constants';
 
 type UserType = {
   id: string,
@@ -21,9 +20,9 @@ export const AppContext = createContext({} as AppContextType);
 
 export const AppContextProvider = props => {
   const [user, setUser] = useState({
-    id: DummyUsers['@occult_686'],
-    avatar: 'https://images.unsplash.com/reserve/eBJIgrh3TCeHf7unLQ5e_sailing-5.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1334&q=80',
-    handle: '@occult_686'
+    id: '',
+    avatar: '',
+    handle: ''
   });
   const [theme, setTheme] = useState(Theme.light.colors);
   const [themeType, setThemeType] = useState(Theme.light.type);
