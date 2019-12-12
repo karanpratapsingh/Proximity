b2c8e7d6ea3dfe140a8de5fc92158493848c4735
Karan Pratap Singh <synester.gates4@gmail.com>
2019-12-12 11:52:09 +0530

    added codepush android

--- android/app/src/main/java/com/proximity/app/MainApplication.java
+++ android/app/src/main/java/com/proximity/app/MainApplication.java
@@ -14,6 +14,7 @@ import io.invertase.firebase.storage.RNFirebaseStoragePackage;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 import io.invertase.firebase.fabric.crashlytics.RNFirebaseCrashlyticsPackage;
+import com.microsoft.codepush.react.CodePush;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -22,6 +23,12 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         public boolean getUseDeveloperSupport() {
           return BuildConfig.DEBUG;
+        }
+
+        // Codepush [CUSTOM]
+        @Override
+        protected String getJSBundleFile() {
+          return CodePush.getJSBundleFile();
         }
 
         @Override
