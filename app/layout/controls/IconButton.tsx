d70e07a081b35d4935e028f554741578f16d1878
Karan Pratap Singh <synester.gates4@gmail.com>
2019-12-16 13:44:02 +0530

    added badge support

--- app/layout/controls/IconButton.tsx
+++ app/layout/controls/IconButton.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import { StyleSheet, TouchableOpacity, StyleProp, ViewStyle } from 'react-native';
+import { StyleProp, StyleSheet, Text, TouchableOpacity, View, ViewStyle } from 'react-native';
+import { ThemeStatic, Typography } from '../../theme';
+
+const { FontWeights, FontSizes } = Typography;
 
 interface IconButtonProps {
   Icon: React.FC,
   onPress: () => void,
-  style?: StyleProp<ViewStyle>
+  style?: StyleProp<ViewStyle>,
+  hasBadge?: boolean
+  badgeCount?: number
 };
 
-const IconButton: React.FC<IconButtonProps> = ({ Icon, onPress, style }) => (
+const IconButton: React.FC<IconButtonProps> = ({ Icon, onPress, style, hasBadge, badgeCount }) => (
   <TouchableOpacity onPress={onPress} activeOpacity={0.95} style={[styles.container, style]}>
     <Icon />
+    {hasBadge && <View style={styles.badge}>
+      <Text style={styles.badgeText}>{badgeCount}</Text>
+    </View>}
   </TouchableOpacity>
 );
 
@@ -17,6 +25,22 @@ const styles = StyleSheet.create({
   container: {
     alignItems: 'flex-end',
     width: 40
+  },
+  badge: {
+    position: 'absolute',
+    height: 20,
+    width: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    top: -10,
+    right: -10,
+    borderRadius: 20,
+    backgroundColor: ThemeStatic.badge
+  },
+  badgeText: {
+    ...FontWeights.Regular,
+    ...FontSizes.Caption,
+    color: ThemeStatic.white
   }
 });
 
