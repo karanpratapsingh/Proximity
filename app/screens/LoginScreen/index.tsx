91925593e9cbfb27c782a6a851f48311f5e6a6e1
Karan Pratap Singh <synester.gates4@gmail.com>
2020-01-18 13:44:20 +0530

    added apple login (alpha)

--- app/screens/LoginScreen/index.tsx
+++ app/screens/LoginScreen/index.tsx
@@ -1,25 +1,27 @@
 import { useMutation } from '@apollo/react-hooks';
+import appleAuth, { AppleAuthCredentialState, AppleAuthRequestOperation, AppleAuthRequestScope, AppleButton } from '@invertase/react-native-apple-authentication';
 import { GoogleSignin } from '@react-native-community/google-signin';
-import React, { useContext, useEffect, useState, useRef } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View, Platform } from 'react-native';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions';
 import SplashScreen from 'react-native-splash-screen';
 import { useNavigation } from 'react-navigation-hooks';
 import GoogleLogo from '../../../assets/svg/google-logo.svg';
 import LoginBanner from '../../../assets/svg/login-banner.svg';
-import { Routes, IconSizes, Errors } from '../../constants';
+import { Errors, IconSizes, Routes } from '../../constants';
 import { AppContext } from '../../context';
 import client from '../../graphql/client';
 import { MUTATION_CREATE_USER } from '../../graphql/mutation';
 import { QUERY_SIGNIN, QUERY_USER_EXISTS } from '../../graphql/query';
-import { Button, LoadingIndicator } from '../../layout';
+import { Button, ConfirmationModal, LoadingIndicator } from '../../layout';
 import { ThemeStatic, Typography } from '../../theme';
 import { ThemeColors } from '../../types/theme';
 import { handleLoginError, signOut } from '../../utils/authentication';
-import { loadToken, saveToken } from '../../utils/storage';
-import TermsAndConditionsBottomSheet from './components/TermsAndConditionsBottomSheet'
 import { crashlytics } from '../../utils/firebase';
 import { welcomeNotification } from '../../utils/notifications';
+import { generateUUID } from '../../utils/shared';
+import { loadToken, saveToken } from '../../utils/storage';
+import TermsAndConditionsBottomSheet from './components/TermsAndConditionsBottomSheet';
 
 const { FontWeights, FontSizes } = Typography;
 
@@ -28,7 +30,10 @@ const LoginScreen: React.FC = () => {
   const { navigate } = useNavigation();
   const [createUser] = useMutation(MUTATION_CREATE_USER);
   const [initializing, setInitializing] = useState(true);
-  const [loading, setLoading] = useState(false);
+  const [googleLoading, setGoogleLoading] = useState(false);
+  const [appleLoading, setAppleLoading] = useState(false);
+  const [termsConfirmationModal, setTermsConfirmationModal] = useState(false);
+  const [authState, setAuthState] = useState({});
 
   const termsAndConditionsBottomSheetRef = useRef();
 
@@ -64,29 +69,104 @@ const LoginScreen: React.FC = () => {
     initialize();
   }, []);
 
+  const termsConfirmationToggle = () => {
+    setTermsConfirmationModal(!termsConfirmationModal);
+  };
+
+  const saveTokenAndNavigate = async (token: string) => {
+    await saveToken(token);
+    setGoogleLoading(false);
+    setAppleLoading(false);
+    setAuthState({});
+    navigateToApp(token);
+  };
+
+  const processNewUser = async () => {
+    termsConfirmationToggle();
+    //@ts-ignore
+    const { token, avatar, name, email } = authState;
+    await createUser({ variables: { token, avatar, name, email } });
+    await saveTokenAndNavigate(token);
+  };
+
+  const processSignIn = async (token: string, avatar: string | null, name: string | null, email: string) => {
+    const { data: { userExists } } = await client.query({ query: QUERY_USER_EXISTS, variables: { token } });
+    if (!userExists) {
+      setAuthState({ token, avatar, name, email });
+      termsConfirmationToggle();
+    }
+    await saveTokenAndNavigate(token);
+  };
+
   const onGoogleSignIn = async () => {
 
-    if (loading) return;
+    if (googleLoading) return;
 
     try {
-      setLoading(true);
+      setGoogleLoading(true);
       const data = await GoogleSignin.signIn();
       const { user: { id: token, name, photo, email } } = data;
 
-      const { data: { userExists } } = await client.query({ query: QUERY_USER_EXISTS, variables: { token } });
-      if (!userExists) {
-        await createUser({ variables: { token: token, avatar: photo, name, email } });
-      }
-      await saveToken(token);
-      setLoading(false);
-      navigateToApp(token);
+      await processSignIn(token, photo, name, email);
     } catch ({ message }) {
-      setLoading(false);
+      setGoogleLoading(false);
+      crashlytics.recordCustomError(Errors.SIGN_IN, message);
+    }
+  };
+
+
+  const onAppleSignIn = async () => {
+
+    if (appleLoading) return;
+
+    try {
+      setAppleLoading(true);
+      const appleAuthRequestResponse = await appleAuth.performRequest({
+        requestedOperation: AppleAuthRequestOperation.LOGIN,
+        requestedScopes: [AppleAuthRequestScope.EMAIL, AppleAuthRequestScope.FULL_NAME],
+      });
+
+      const credentialState = await appleAuth.getCredentialStateForUser(appleAuthRequestResponse.user);
+
+      if (credentialState === AppleAuthCredentialState.AUTHORIZED) {
+
+        alert(JSON.stringify(appleAuthRequestResponse));
+        return;
+        const token = generateUUID();
+        const { email, fullName } = appleAuthRequestResponse;
+        // @ts-ignore
+        const { givenName, middleName, familyName } = fullName;
+        const name = `${givenName} ${middleName} ${familyName}`.trim();
+        // @ts-ignore
+        await processSignIn(token, '', name, email);
+      }
+    } catch ({ message }) {
+      setAppleLoading(false);
       crashlytics.recordCustomError(Errors.SIGN_IN, message);
     }
   };
 
   let content = <LoadingIndicator color={ThemeStatic.accent} size={IconSizes.x1} />;
+  let appleSignInButton;
+
+  if (Platform.OS === 'ios') {
+    if (appleLoading) {
+      appleSignInButton = (
+        <View style={styles().loadingAppleLogin}>
+          <LoadingIndicator color={ThemeStatic.accent} size={IconSizes.x0} />
+        </View>
+      );
+    } else {
+      appleSignInButton = (
+        <AppleButton
+          style={styles().appleSignIn}
+          buttonStyle={AppleButton.Style.BLACK}
+          buttonType={AppleButton.Type.SIGN_IN}
+          onPress={() => onAppleSignIn()}
+        />
+      );
+    }
+  }
 
   if (!initializing) {
     content = (
@@ -109,8 +189,9 @@ const LoginScreen: React.FC = () => {
               containerStyle={styles(theme).loginButton}
               labelStyle={styles(theme).loginButtonText}
               indicatorColor={ThemeStatic.accent}
-              loading={loading}
+              loading={googleLoading}
             />
+            {appleSignInButton}
             <TouchableOpacity
               // @ts-ignore
               onPress={termsAndConditionsBottomSheetRef.current.open}
@@ -127,6 +208,15 @@ const LoginScreen: React.FC = () => {
     <View style={styles(theme).container}>
       {content}
       <TermsAndConditionsBottomSheet ref={termsAndConditionsBottomSheetRef} />
+      <ConfirmationModal
+        label='Confirm'
+        title='Terms and Conditions'
+        description={`By clicking confirm you agree to our terms and conditions`}
+        color={ThemeStatic.accent}
+        isVisible={termsConfirmationModal}
+        toggle={termsConfirmationToggle}
+        onConfirm={processNewUser}
+      />
     </View>
   );
 };
@@ -155,7 +245,7 @@ const styles = (theme = {} as ThemeColors) => StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'space-between',
-    paddingTop: responsiveHeight(12),
+    paddingTop: responsiveHeight(Platform.select({ ios: 10, android: 12 })),
     paddingBottom: 16,
   },
   loginButton: {
@@ -172,6 +262,17 @@ const styles = (theme = {} as ThemeColors) => StyleSheet.create({
     ...FontSizes.Body,
     marginLeft: 10,
     color: theme.accent
+  },
+  appleSignIn: {
+    height: 44,
+    width: responsiveWidth(90),
+    marginBottom: 10
+  },
+  loadingAppleLogin: {
+    height: 44,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10,
   },
   terms: {
     alignItems: 'center',
