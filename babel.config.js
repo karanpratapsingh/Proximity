bca786ed31e01a7a5e6ee24dba6f4dd8a3373503
Karan Pratap Singh <synester.gates4@gmail.com>
2020-03-08 10:42:34 +0530

    setup module resolution

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,35 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@app/config/*': './app/config/*',
+          '@app/context/*': './app/context/*',
+          '@app/graphql/*': './app/graphql/*',
+          '@app/constants/*': './app/constants/*',
+          '@app/layout/*': './app/layout/*',
+          '@app/navigation/*': './app/navigation/*',
+          '@app/screens/*': './app/screens/*',
+          '@app/theme/*': './app/theme/*',
+          '@app/types/*': './app/types/*',
+          '@app/utils/*': './app/utils/*',
+          '@app/assets/*': './assets/*',
+        },
+      },
+    ],
+  ],
 };
