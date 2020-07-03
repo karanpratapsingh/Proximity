082e920cb33dfe2dde2d2e840dbad22aeff55033
Karan Pratap Singh <synester.gates4@gmail.com>
2020-07-03 19:59:11 +0530

    migrate to firebase auth

--- app/utils/firebase.ts
+++ app/utils/firebase.ts
@@ -1,20 +1,67 @@
-import firebase from 'react-native-firebase';
+import firebase, { AuthCredential } from 'react-native-firebase';
 import { generateUUID } from './shared';
-import { Asset, StoragePaths, Errors } from '@app/constants';
+import { Asset, StoragePaths, Errors, AuthDefaults } from '@app/constants';
 import { Platform } from 'react-native';
+import { SocialSignInType, SocialSignInResult } from '@app/types/auth';
 
 export const storage = firebase.storage();
 export const messaging = firebase.messaging();
 export const notifications = firebase.notifications();
 export const crashlytics = firebase.crashlytics();
+export const auth = firebase.auth();
+
+const getGoogleCredentials = (idToken: string | null): AuthCredential =>
+  firebase.auth.GoogleAuthProvider.credential(idToken);
+
+const getAppleCredentials = (
+  identityToken: string | null,
+  nonce: string,
+): AuthCredential =>
+  firebase.auth.AppleAuthProvider.credential(identityToken, nonce);
+
+export const processSocialSignIn = async (
+  authResult: any,
+  type: SocialSignInType,
+): Promise<SocialSignInResult> => {
+  let credentials: AuthCredential;
+
+  if (type === SocialSignInType.GOOGLE) {
+    const { idToken } = authResult;
+    credentials = getGoogleCredentials(idToken);
+  } else {
+    const { identityToken, nonce } = authResult;
+    credentials = getAppleCredentials(identityToken, nonce);
+  }
+
+  const {
+    user: { providerData, providerId },
+  } = await auth.signInWithCredential(credentials);
+  const [
+    { uid: token, displayName, photoURL, email },
+  ] = providerData;
+
+  const name = displayName || AuthDefaults.name;
+  const avatar = photoURL || AuthDefaults.avatar;
+  const safeEmail = email || `${token}@${providerId}`;
+
+  const socialSignInResult = {
+    token,
+    name,
+    avatar,
+    email: safeEmail,
+  };
+  
+  return socialSignInResult;
+};
 
 export const initializeFCM = async () => {
   try {
     if (Platform.OS === 'android') {
-      const channel = new firebase
-        .notifications
-        .Android
-        .Channel('proximity-channel', 'Notification Channel', firebase.notifications.Android.Importance.Max)
+      const channel = new firebase.notifications.Android.Channel(
+        'proximity-channel',
+        'Notification Channel',
+        firebase.notifications.Android.Importance.Max,
+      )
         .setDescription('Proximity Notification Channel')
         .setSound('default');
 
@@ -34,7 +81,7 @@ export const initializeFCM = async () => {
 
 export const uploadToStorage = (asset: string, uri: string, userId: string) => {
   const [type] = getMediaType(uri);
-  let storageRef;
+  let storageRef: string | undefined;
 
   if (asset === Asset.avatar) {
     storageRef = `${StoragePaths.avatars}/${userId}.${type}`;
@@ -42,11 +89,10 @@ export const uploadToStorage = (asset: string, uri: string, userId: string) => {
     storageRef = `${StoragePaths.posts}/${userId}/${generateUUID()}.${type}`;
   }
 
-  return storage
-    .ref(storageRef)
-    .putFile(uri);
+  return storage.ref(storageRef).putFile(uri);
 };
 
 export const getMediaType = (uri: string) => uri.split('.').slice(-1);
 
-export const deleteFromStorage = (uri: string) => storage.refFromURL(uri).delete();
+export const deleteFromStorage = (uri: string) =>
+  storage.refFromURL(uri).delete();
