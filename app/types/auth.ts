2a0ef90f7a092dc1efab4c7d9076df11b618f129
Karan Pratap Singh <synester.gates4@gmail.com>
2020-07-03 19:34:11 +0530

    added auth types

---
+++ app/types/auth.ts
@@ -0,0 +1,20 @@
+export type GoogleAuthResult = {
+  idToken: string | null;
+};
+
+export type AppleAuthResult = {
+  identityToken: string | null;
+  nonce: string;
+};
+
+export enum SocialSignInType {
+  GOOGLE,
+  APPLE,
+}
+
+export type SocialSignInResult = {
+  token: string;
+  avatar: string | null;
+  name: string;
+  email: string;
+};
