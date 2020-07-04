9463d66d36bcc4ab58a45ce1e33415c1974010c3
Karan Pratap Singh <synester.gates4@gmail.com>
2020-07-05 00:42:29 +0530

    add reverse explore grid card

--- app/screens/ExploreScreen/components/ExploreGrid.tsx
+++ app/screens/ExploreScreen/components/ExploreGrid.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { StyleSheet, View, RefreshControl } from 'react-native';
 import { FlatGrid } from 'react-native-super-grid';
 import { ListEmptyComponent } from '@app/layout';
@@ -16,17 +16,15 @@ interface ExploreGridProps {
 const ExploreGrid: React.FC<ExploreGridProps> = ({ posts, onRefresh, tintColor }) => {
 
   const renderItem = ({ item, index }) => {
-    let content: React.ReactElement = <PrimaryImageGroup imageGroup={item} />
+    const isSecondaryImageGroup = index % 3 === 2;
 
-    if (index % 3 === 2) {
-      content = <SecondaryImageGroup imageGroup={item} />;
+    if (isSecondaryImageGroup) {
+      const reversed = index % 2 === 1;
+
+      return <SecondaryImageGroup reversed={reversed} imageGroup={item} />;
     }
 
-    return (
-      <View style={styles.gridImageGroup}>
-        {content}
-      </View>
-    );
+    return <PrimaryImageGroup imageGroup={item} />
   };
 
   const refreshControl = () => (
@@ -59,12 +57,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
-  },
-  gridImageGroup: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between'
   }
 });
 
